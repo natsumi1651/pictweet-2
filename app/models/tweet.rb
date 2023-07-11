11fe59dcdf2a98bc4263514bbb2c17400370c925
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-11 17:56:43 +0900

    検索機能

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,16 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
+<<<<<<< Updated upstream
+=======
+  has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
+<<<<<<< Updated upstream
   resources :tweets
+=======
+  resources :tweets do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
+>>>>>>> Stashed changes
   resources :users, only: :show
 end
